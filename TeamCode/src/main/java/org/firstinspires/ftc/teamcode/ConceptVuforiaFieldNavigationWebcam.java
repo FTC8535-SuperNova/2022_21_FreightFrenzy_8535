e888503f27ddc8c0982a626eaca24c0482189bbd
Td C <tdc302@gmail.com>
2021-12-07 20:35:36 -0500

    Added the control for the motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptVuforiaFieldNavigationWebcam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptVuforiaFieldNavigationWebcam.java
@@ -32,11 +32,13 @@ package org.firstinspires.ftc.teamcode;
 
 import static java.lang.Math.*;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.util.RobotLog;
 
 
 /**
@@ -63,46 +65,70 @@ import com.qualcomm.robotcore.util.Range;
  * is explained below.
  */
 
-@TeleOp(name="Vuforia Field Nav Webcam", group ="Concept")
+@Autonomous(name="Vuforia Field Nav Webcam", group ="Concept")
 
 public class ConceptVuforiaFieldNavigationWebcam extends LinearOpMode {
 
 
-    private Vuforia vuforia = new Vuforia(true,"AQq/tAz/////AAABmQLdTKfIqUYXtldONwoyIpt7LuTPID2/eRq05VBghKTN/JlWD5muCZjHl0d0gYu9jlfUdMyvqJRVGPVlqDouTF1bcS9DMTB1LMFNPhHmQspaaqs2MkDFxDScBncQLegWEaJfm4oiuIwAK18SHeHqg8yNoH2LQZa/HoobNlZME5Xm/YpwK8oxv+1qEMezceJ03ANFTpbPDFFmD8rOGR79Ms6/p1O1i+S8ZKrujs9E5r9pHQrD7zCr8o2bxCha3PIFyX9SS4sxeHIX4/W9Bjd++0Tb3aU7MV607WwkgQvZKYPN27X/y3f6JyiCzQFwZMpbMRtSMczsAs3zPFA9C8ZDuq+d4bL023dshULTmnUeWVzT\n");
+    private Vuforia vuforia = new Vuforia(false,"AQq/tAz/////AAABmQLdTKfIqUYXtldONwoyIpt7LuTPID2/eRq05VBghKTN/JlWD5muCZjHl0d0gYu9jlfUdMyvqJRVGPVlqDouTF1bcS9DMTB1LMFNPhHmQspaaqs2MkDFxDScBncQLegWEaJfm4oiuIwAK18SHeHqg8yNoH2LQZa/HoobNlZME5Xm/YpwK8oxv+1qEMezceJ03ANFTpbPDFFmD8rOGR79Ms6/p1O1i+S8ZKrujs9E5r9pHQrD7zCr8o2bxCha3PIFyX9SS4sxeHIX4/W9Bjd++0Tb3aU7MV607WwkgQvZKYPN27X/y3f6JyiCzQFwZMpbMRtSMczsAs3zPFA9C8ZDuq+d4bL023dshULTmnUeWVzT\n");
+
+    public int hasError = 0;
     private RobotHardware robot = new RobotHardware();
     private ElapsedTime runtime  = new ElapsedTime();
 
-    private void vuforiaDriveToPos(double targetX, double targetZ) {
+    private AutoDriveByGyro_Instantiated gyroDriving = new AutoDriveByGyro_Instantiated(hardwareMap);
+
+
+    /** A method that uses cool math, vuforia, and the gyro to drive to a specific location relative to the field, not the robot.
+
+
+     */
+    private void vuforiaDriveToPos(double targetX, double targetZ, double power, double finalAngle) {
 
         vuforia.updateVuforia();
-        double currentX = vuforia.translation.get(0);
-        double currentZ = vuforia.translation.get(2);
-        double xTranslation = currentX - targetX;
-        double yTranslation = currentZ - targetZ;
+        double currentX = 1;
+        double currentZ = 1;
+        try {
+            currentX = vuforia.translation.get(0);
+            currentZ = vuforia.translation.get(2);
 
-        double targetRange = Math.hypot(xTranslation, yTranslation);
+        } catch(Exception NullPointerException){
+            RobotLog.addGlobalWarningMessage("Called vuforia.translation.get(int); without a visible target!");
+            hasError = 1;
+
+            return;
+        }
+        double xTranslation = currentX - targetX;
+        double zTranslation = currentZ - targetZ;
+
+        // Target distance is in inches
+        double targetRange = Math.hypot(xTranslation, zTranslation);
 
         // target bearing is based on angle formed between the X axis and the target range line
         double targetBearing = Math.toDegrees(Math.asin(targetX / targetRange));
+        gyroDriving.gyroDrive(power, targetRange, targetBearing);
 
-        robot.leftDrive.setPower(Range.clip(targetRange+targetBearing, -1,1));
-        robot.rightDrive.setPower(Range.clip(targetRange-targetBearing, -1,1));
+        if(finalAngle != -1) {
+            gyroDriving.gyroTurn(power, targetBearing);
 
 
+        }
     }
+
 
     @Override public void runOpMode()
     {
         vuforia.initVuforia();
 
-
         robot.init(hardwareMap);
+
+        gyroDriving.init();
 
         waitForStart();
         runtime.reset();
 
 
-        vuforiaDriveToPos(6,7);
+        vuforiaDriveToPos(0,0,1,1);
 
     }
 }
